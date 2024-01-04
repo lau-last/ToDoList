6798b0456c0fce9b2f3a7acb79c93bdeb20f9b64
lau-last <laurent.last7@gmail.com>
2024-01-04 16:44:50 +0100

    first symfony 4.4

---
+++ config/bundles.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+];
